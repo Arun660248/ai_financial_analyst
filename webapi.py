b3b12d30316eb88e455877de71c44fd2f7e74b16
Arun <arunjyotichakraborty18@gmail.com>
2026-06-02 12:08:48 +0530

    feat: build decoupled streamlit client and fix api state management

--- webapi.py
+++ webapi.py
@@ -1,3 +1,4 @@
+import os
 from data_fetcher import agent_executor
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
@@ -9,12 +10,20 @@ class ChatRequest(BaseModel):
     query: str
 @app.post("/analyze")
 async def analyze_query(request: ChatRequest):
+    if os.path.exists("static/financial_chart.png"):
+        os.remove("static/financial_chart.png")
     result = agent_executor.invoke({"messages": [("user", request.query)]})
     extracted_text = result["messages"][-1].content
+    if os.path.exists("static/financial_chart.png"):
 
-    return {"status": "success",
+        return {"status": "success",
+            "answer": extracted_text,
+            "chart_url": "http://localhost:8000/static/financial_chart.png"}
+    else:
+        return {"status": "success",
         "answer": extracted_text,
-        "chart_url": "http://localhost:8000/static/financial_chart.png"}
+        "chart_url": None}
+
 
 
 
